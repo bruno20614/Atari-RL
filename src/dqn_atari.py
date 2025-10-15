383df5e776190da44b6ea9da70e25222aa6a75d9
Bruno Costa <brunocdl9@gmail.com>
2025-10-15 17:23:10 -0300

    finalizado as importações das bibliotecas

--- src/dqn_atari.py
+++ src/dqn_atari.py
@@ -1,9 +1,24 @@
 import torch
-import torch as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
-
-class teste(nn.Module):
-    def __init__():
-        return print("Hello World")
+import torch.nn as nn 
+import numpy as np
+import pandas as pd
+import gymnasium as gym
+import ale_py
+import argparse
+import os
+import random
+import time
+from distutils.util import strtobool
+from ale_py import AtariEnv
+from ale_py import ALEInterface
+from stable_baselines3.common.atari_wrappers import (
+    ClipRewardEnv,
+    EpisodicLifeEnv,
+    FireResetEnv,
+    MaxAndSkipEnv,
+    NoopResetEnv
+)
+from stable_baselines3.common.buffers import ReplayBuffer
+from torch.utils.tensorboard import SummaryWriter
