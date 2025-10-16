02de8055bb0fab2fdc928ccf1272512e18be1613
Bruno Costa <brunocdl9@gmail.com>
2025-10-16 19:04:37 -0300

    Atualizado o gitignore

---
+++ src/train.sh
@@ -0,0 +1,9 @@
+python3 dqn_atari.py \
+    --exp-name MsPacman-v5 \
+    --track \
+    --wandb-project-name ALE \
+    --capture-video \
+    --env-id ALE/MsPacman-v5 \
+    --total-timesteps 5000000 \
+    --buffer-size 400000 \
+ 
